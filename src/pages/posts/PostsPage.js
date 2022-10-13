d7a3bcea1a9ab09c96582648e8a9b269e629dd33
Vilayat Kleer <vkleer@hotmail.com>
2022-10-13 20:47:35 +0000

    Create PopularProfiles component and set up basic structure

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -14,6 +14,7 @@ import NoResults from '../../assets/no-results.png';
 import Asset from "../../components/Asset";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 function PostsPage({message, filter = ''}) {
   const [posts, setPosts] = useState({ results: [] });
@@ -82,7 +83,7 @@ function PostsPage({message, filter = ''}) {
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+        <PopularProfiles />
       </Col>
     </Row>
   );
---
+++ src/pages/profiles/PopularProfiles.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import appStyles from '../../App.module.css';
+import { Container } from 'react-bootstrap';
+import { axiosReq } from '../../api/axiosDefaults';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import Asset from '../../components/Asset';
+
+const PopularProfiles = () => {
+    const [profileData, setProfileData] = useState({
+        pageProfile: { results: [] },
+        popularProfiles: { results: [] },
+    });
+    const { popularProfiles } = profileData;
+    const currentUser = useCurrentUser();
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const { data } = await axiosReq.get(
+                    '/profiles/?ordering=-followers_count'
+                );
+                setProfileData(prevState => ({
+                    ...prevState,
+                    popularProfiles: data,
+                }));
+            } catch(err) {
+                console.log(err);
+            }
+        };
+
+        handleMount();
+    }, [currentUser]);
+
+  return (
+        <Container className={appStyles.Content}>
+            {popularProfiles.results.length ? (
+                <>
+                    <p>Most followed profiles</p>
+                    {popularProfiles.results.map(profile => (
+                        <p key={profile.id}>{profile.owner}</p>
+                    ))}
+                </>
+            ) : (
+                <Asset spinner />
+            )}
+        </Container>
+    );
+};
+
+export default PopularProfiles
