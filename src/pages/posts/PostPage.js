58d17407ab61bc6c1565dcca6e5155a6e6619133
Vilayat Kleer <vkleer@hotmail.com>
2022-10-13 20:33:28 +0000

    Add infinite scrolling to comments

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -12,30 +12,33 @@ import Post from "./Post";
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Comment from "../comments/Comment";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostPage() {
-    const { id } = useParams();
-    const [post, setPost] = useState({ results: [] });
-    const currentUser = useCurrentUser();
+  const { id } = useParams();
+  const [post, setPost] = useState({ results: [] });
+  const currentUser = useCurrentUser();
   const profile_image = currentUser?.profile_image;
   const [comments, setComments] = useState({ results: [] });
 
-    useEffect(() => {
-        const handleMount = async () => {
-        try {
-            const [{data: post}, {data: comments}] = await Promise.all([
-            axiosReq.get(`/posts/${id}`),
-            axiosReq.get(`/comments/?post=${id}`)
-            ]);
-            setPost({results: [post]});
-            setComments(comments);
-        } catch(err) {
-            console.log(err);
-        }
-        }
+  useEffect(() => {
+      const handleMount = async () => {
+      try {
+          const [{data: post}, {data: comments}] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`)
+          ]);
+          setPost({results: [post]});
+          setComments(comments);
+      } catch(err) {
+          console.log(err);
+      }
+      }
 
-        handleMount();
-    }, [id]);
+      handleMount();
+  }, [id]);
 
   return (
     <Row className="h-100">
@@ -55,11 +58,19 @@ function PostPage() {
             "Comments"
           ) : null}
           {comments.results.length ? (
-            comments.results.map(comment => (
-              <Comment key={comment.id} {...comment} 
-              setPost={setPost} setComments={setComments}
-              />
-            ))
+            <InfiniteScroll
+              children={comments.results.map(comment => (
+                <Comment 
+                  key={comment.id} {...comment} 
+                  setPost={setPost} 
+                  setComments={setComments}
+                />
+              ))}
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+            />
           ) : currentUser ? (
             <span>No comments yet - be the first to comment!</span>
           ) : (
