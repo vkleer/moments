a142f103826dbebbef9d6880918de513898f978f
Vilayat Kleer <vkleer@hotmail.com>
2022-10-15 05:14:29 +0000

    Deployment commit

--- src/pages/posts/PostEditForm.js
+++ src/pages/posts/PostEditForm.js
@@ -34,7 +34,7 @@ function PostEditForm() {
 
                 is_owner ? setPostData({title, content, image}) : history.push('/');
             } catch(err) {
-                console.log(err);
+                // console.log(err);
             }
         };
         
@@ -75,7 +75,7 @@ function PostEditForm() {
             await axiosReq.put(`/posts/${id}/`, formData);
             history.push(`/posts/${id}`);
         } catch (err) {
-            console.log(err);
+            // console.log(err);
             if (err.response?.status !== 401) {
                 setErrors(err.response?.data);
             }
